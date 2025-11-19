0a545cb1243b32a1d6775ca75ff09a329dde6f5b
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-11-19 13:21:18 -0700

    bash

--- nightly.sh
+++ nightly.sh
@@ -2,6 +2,6 @@ set -e -x
 if [ ! -d "out/Debug" ]; then
     python3 tools/git-sync-deps
 fi
-./bin/gn gen out/Debug --args='cc="clang" cxx="clang++ extra_cflags_cc=["-frtti"]"'
+./bin/gn gen out/Debug --args='cc="clang" cxx="clang++" extra_cflags_cc=["-frtti"]'
 ninja -C out/Debug easteregg
 ./out/Debug/easteregg --input test.skp
