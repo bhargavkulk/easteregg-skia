3944795b5d5cd76f5ddfa8fac61ce82824085ed2
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-11-18 21:13:28 -0700

    make sure not to rerun deps

--- nightly.sh
+++ nightly.sh
@@ -1,4 +1,6 @@
 set -e -x
-python3 tools/git-sync-deps
+if [ ! -d "out/Debug" ]; then
+    python3 tools/git-sync-deps
+fi
 ./bin/gn gen out/Debug --args='cc="clang" cxx="clang++"'
 ninja -C out/Debug easteregg
