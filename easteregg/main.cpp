85483780a5dab8c519aae9f61de9094357228088
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-11-19 12:48:21 -0700

    testing record gen

--- easteregg/main.cpp
+++ easteregg/main.cpp
@@ -1,10 +1,13 @@
+#include <cstdio>
 #include "include/core/SkCanvas.h"
 #include "include/core/SkImage.h"
 #include "include/core/SkPicture.h"
+#include "include/core/SkRecorder.h"
 #include "include/core/SkStream.h"
-#include "include/core/SkSurface.h"
-#include "include/encode/SkPngEncoder.h"
+#include "src/core/SkRecord.h"
 #include "tools/flags/CommandLineFlags.h"
+
+#define ERROR(fmt, ...) fprintf(stderr, "Error: " fmt "\n", ##__VA_ARGS__)
 
 static DEFINE_string(input, "", "Input .skp file");
 static DEFINE_string(output, "output.png", "Output PNG file");
@@ -13,35 +16,28 @@ int main(int argc, char** argv) {
     CommandLineFlags::Parse(argc, argv);
 
     if (FLAGS_input.isEmpty()) {
-        SkDebugf("Must specify --input\n");
+        ERROR("Must specify --input");
         return 1;
     }
 
     SkFILEStream stream(FLAGS_input[0]);
     if (!stream.isValid()) {
-        SkDebugf("Failed to open %s\n", FLAGS_input[0]);
+        ERROR("Failed to read file %s", FLAGS_input[0]);
         return 1;
     }
 
-    sk_sp<SkPicture> picture = SkPicture::MakeFromStream(&stream);
+    sk_sp<SkPicture> picture(SkPicture::MakeFromStream(&stream));
     if (!picture) {
-        SkDebugf("Failed to deserialize picture\n");
+        ERROR("Error loading skp from %s", FLAGS_input[0]);
         return 1;
     }
 
-    SkRect bounds = picture->cullRect();
-    auto surface = SkSurfaces::Raster(SkImageInfo::MakeN32Premul(bounds.width(), bounds.height()));
+    SkRect bounds(picture->cullRect());
+    SkRecord records;
+    SkRecorder recorder(&records, bounds.width(), bounds.height());
+    picture->playback(&recorder);
 
-    SkCanvas* canvas = surface->getCanvas();
-    canvas->clear(SK_ColorWHITE);
-    canvas->drawPicture(picture);
-
-    sk_sp<SkImage> image = surface->makeImageSnapshot();
-    SkPixmap pixmap;
-    image->peekPixels(&pixmap);
-
-    SkFILEWStream outStream(FLAGS_output[0]);
-    SkPngEncoder::Encode(&outStream, pixmap, {});
+    printf("Record has %d commands.\n", records.count());
 
     return 0;
 }
