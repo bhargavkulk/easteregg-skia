2356a41f706aa9efb672a6f0de92d92d7f0c7751
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-11-19 15:38:26 -0700

    making the rectangles res

--- easteregg/main.cpp
+++ easteregg/main.cpp
@@ -1,10 +1,18 @@
 #include <cstdio>
+#include <sstream>
 #include "include/core/SkCanvas.h"
+#include "include/core/SkColor.h"
 #include "include/core/SkImage.h"
+#include "include/core/SkImageInfo.h"
 #include "include/core/SkPicture.h"
+#include "include/core/SkPixmap.h"
 #include "include/core/SkStream.h"
+#include "include/core/SkSurface.h"
+#include "include/encode/SkPngEncoder.h"
 #include "src/core/SkRecord.h"
 #include "src/core/SkRecordCanvas.h"
+#include "src/core/SkRecordDraw.h"
+#include "src/core/SkRecords.h"
 #include "tools/flags/CommandLineFlags.h"
 
 #define ERROR(fmt, ...) fprintf(stderr, "Error: " fmt "\n", ##__VA_ARGS__)
@@ -13,15 +21,50 @@ static DEFINE_string(input, "", "Input .skp file");
 static DEFINE_string(output, "index.html", "Output LOG file");
 
 struct RecordPrinter {
-    FILE* file;
+    std::ostringstream os;
     int index = 0;
 
-    RecordPrinter(FILE* file) : file(file) {}
-
     template <typename T> void operator()(const T& op) {
-        fprintf(file, "    [%d] %s<br />", index++, typeid(T).name());
+        os << "    [" << index++ << "] " << typeid(T).name() << "<br />\n";
+    }
+
+    std::string str() const { return os.str(); }
+};
+
+struct DrawRectRed {
+    template <typename T> void operator()(T* op) {
+        // Do nothing for anything other than DrawRect
+    }
+
+    void operator()(SkRecords::DrawRect* op) {
+        auto& paint = op->paint;
+        if (!paint.getShader() && !paint.getColorFilter() && !paint.getImageFilter() &&
+            !paint.getMaskFilter()) {
+            SkColor4f color = op->paint.getColor4f();
+            color.fR = 1.0;
+            color.fG = 0.0;
+            color.fB = 0.0;
+        }
     }
 };
+
+// Draws a given SkRecord `records` into a png file named `filename`
+bool drawRecordToFile(const SkRecord& records, const SkRect& bounds, const char* filename) {
+    auto surface = SkSurfaces::Raster(SkImageInfo::MakeN32Premul(bounds.width(), bounds.height()));
+
+    SkCanvas* canvas = surface->getCanvas();
+    canvas->clear(SK_ColorWHITE);
+    SkRecordDraw(records, canvas, nullptr, nullptr, 0, nullptr, nullptr);
+
+    sk_sp<SkImage> image = surface->makeImageSnapshot();
+    SkPixmap pixmap;
+    image->peekPixels(&pixmap);
+
+    SkFILEWStream stream(filename);
+    if (!stream.isValid()) return false;
+    SkPngEncoder::Encode(&stream, pixmap, {});
+    return true;
+}
 
 int main(int argc, char** argv) {
     CommandLineFlags::Parse(argc, argv);
@@ -51,16 +94,35 @@ int main(int argc, char** argv) {
 
     printf("Record has %d commands.\n\n", records.count());
 
+    RecordPrinter printer;
+    for (int i = 0; i < records.count(); i++) {
+        records.visit(i, printer);
+    }
+
+    drawRecordToFile(records, bounds, "before.png");
+
+    DrawRectRed mutator;
+    for (int i = 0; i < records.count(); i++) {
+        records.mutate(i, mutator);
+    }
+
+    drawRecordToFile(records, bounds, "after.png");
+
     FILE* outFile = fopen(FLAGS_output[0], "w");
     if (!outFile) {
         ERROR("Failed to open output file %s", FLAGS_output[0]);
         return 1;
     }
 
-    RecordPrinter printer(outFile);
-    for (int i = 0; i < records.count(); i++) {
-        records.visit(i, printer);
-    }
+    fprintf(outFile, "<!DOCTYPE html>\n");
+    fprintf(outFile, "<html><head><title>SKP Comparison</title></head><body>\n");
+    fprintf(outFile, "<h1>Record Commands (%d total)</h1>\n", records.count());
+    fprintf(outFile, "<pre>%s</pre>\n", printer.str().c_str());
+    fprintf(outFile, "<h1>Before Mutation</h1>\n");
+    fprintf(outFile, "<img src='before.png' />\n");
+    fprintf(outFile, "<h1>After Mutation</h1>\n");
+    fprintf(outFile, "<img src='after.png' />\n");
+    fprintf(outFile, "</body></html>\n");
 
     fclose(outFile);
 
