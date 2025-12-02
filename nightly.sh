dbab7d9ba94f3aea1258c861d1c2ebf0d89566fc
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-12-02 13:49:44 -0700

    Revert "new thing"
    This reverts commit a136942685fc36273757a06deff41844330d6df7.

--- nightly.sh
+++ nightly.sh
@@ -5,8 +5,3 @@ fi
 ./bin/gn gen out/Debug --args='cc="clang" cxx="clang++" extra_cflags_cc=["-frtti"]'
 ninja -C out/Debug easteregg
 ./out/Debug/easteregg --input ./test.skp --output ./report/
-python3 easteregg/report_generator.py \
-    --data ./report/report_data.txt \
-    --before ./report/before.png \
-    --after ./report/after.png \
-    --output ./report/index.html
