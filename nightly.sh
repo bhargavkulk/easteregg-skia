d7a8c33aa686e8e20e80476dc720f5c9c99c371e
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2025-12-27 21:16:09 -0700

    saving

--- nightly.sh
+++ nightly.sh
@@ -31,5 +31,5 @@ ninja -C out/Debug optimizer nanobench renderer skp_parser print_bounds
 
 python3 -m venv .venv/
 
-$(pwd)/venv/bin/python -m pip install uv
-$(pwd)/venv/bin/python -m uv sync
+$(pwd)/.venv/bin/python -m pip install uv
+$(pwd)/.venv/bin/python -m uv sync
