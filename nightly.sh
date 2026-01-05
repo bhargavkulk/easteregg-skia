e777ae9db6c15bc389f495fe22fefdac704d7e1d
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2026-01-05 13:58:50 -0700

    dumb mistake again

--- nightly.sh
+++ nightly.sh
@@ -68,8 +68,8 @@ ls old_bench
 
 ls old_jsons
 
-# python3 -m venv venv
-# source venv/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 
 # pip install -r requirements.txt
 # python scripts/benchmark_gen.py old_bench/ \
