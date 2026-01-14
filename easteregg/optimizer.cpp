f29672aa432b46c7a310250bf610aa2aa961a0be
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2026-01-13 20:35:48 -0700

    Enabling GradientMask.

--- easteregg/optimizer.cpp
+++ easteregg/optimizer.cpp
@@ -95,6 +95,7 @@ int main(int argc, char** argv) {
         RemoveLoneLuma opt2;
         GradientDstInToMasks opt3;
 
+        opt3.transform(&records);
         opt2.transform(&records);
         opt1.transform(&records);
 
